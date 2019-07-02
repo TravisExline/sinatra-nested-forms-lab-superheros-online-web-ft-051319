fb12f0a28eb5ae5857b168e7dd7cfcecf9308f63
Travis Exline <t.exline22@gmail.com>
2019-07-02 13:48:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class App < Sinatra::Base
     get '/' do
       erb :super_hero
     end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:superheros].each do |details|
+        Superhero.new(details)
+    end
 end
